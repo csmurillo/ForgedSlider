5350af844ab678e6fa0c96d9d3b583afa56c4b7a
csmurillo <csmurillo00@gmail.com>
2021-04-15 16:41:21 -0700

    version 0: first files

---
+++ js/forged-slider.js
@@ -0,0 +1,27 @@
+// Options
+// width: int
+// height: int
+// moveSlideNitems: int
+// Future AddOns:
+// draggable: boolean
+// snapToNearestBorder: boolean
+// roundabout: boolean
+function initForgedSlider(slider,options){
+
+}
+function moveLeftForgedSlider(){
+
+}
+function moveRightForgedSlider(){
+
+}
+function addItemStartForgedSlider(sliderItem){
+
+}
+function addItemEndForgedSlider(sliderItem){
+
+}
+function addItemPosForgedSlider(sliderItem,pos){
+    
+}
+
